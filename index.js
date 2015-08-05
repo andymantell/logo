c2b45462fca44c792bb5357f6208f2a0c482869c
Andy Mantell <contact@andymantell.com>
2015-08-05 14:00:11 +0100

    Fix paths

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fs = require('fs');
+var path = require('path');
 
-module.exports = fs.readFileSync('dist/andy-mantell.svg', { encoding: 'utf-8' });
+module.exports = fs.readFileSync(path.resolve('dist/andy-mantell.svg'), { encoding: 'utf-8' });
